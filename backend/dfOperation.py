434b4863e94f9217f41bc9aa46a817bd80f61924
ambikeshwar1991 <srivastava.ambikeshwar@gmail.com>
2019-05-13 00:54:08 +0530

    refactored

--- backend/dfOperation.py
+++ backend/dfOperation.py
@@ -4,10 +4,8 @@ import pandas as pd
 def getSeriesData(tickerID, bankName, startDate, endDate):
     results = crawlThread(tickerID, bankName, startDate, endDate)
     df = pd.DataFrame(results)
-    print(df)
     df = df.fillna(method='bfill')
     df = df.fillna(method='ffill')
-    print('Here I am ')
     series = df[df.columns[0]]
     for col in df.columns[1:]:
         series += df[col]
--- backend/server.py
+++ backend/server.py
@@ -3,21 +3,20 @@ from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
-from utils import participant
+from utils import participant, banks
 from dfOperation import getSeriesData
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 class ShareHoldingData(Resource):
-    def get(self, tickerID, bankName, startDate, endDate):
+    def get(self, tickerID, startDate, endDate):
         outList = []
-        for bank in participant.keys():
+        for bank in banks:
             outList.append(getSeriesData(tickerID, bank, startDate, endDate))
-        print(outList)
         return jsonify(outList)
 
-api.add_resource(ShareHoldingData, '/shareHolding/<tickerID>/<bankName>/<startDate>/<endDate>') # Route_1
+api.add_resource(ShareHoldingData, '/shareHolding/<tickerID>/<startDate>/<endDate>') # Route_1
 
 if __name__ == '__main__':
     app.run(port=5002)
